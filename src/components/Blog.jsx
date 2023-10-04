74ce45b7bcd26f99fb1bf70281c7f505ae6f85df
larcondo <larcondo90@gmail.com>
2023-10-04 15:30:19 -0300

    exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} <b>{blog.author}</b>
-  </div>  
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false) 
+
+  const blogStyle = {
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 4,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggle = () => setShowDetails(!showDetails)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <b>{blog.author}</b> <BlogButton showDetails={showDetails} toggle={toggle} />
+        
+      </div>
+      { showDetails && 
+        <div>
+          <p style={{margin: '0'}}>{blog.url}</p>
+          <p style={{margin: '0'}}>likes {blog.likes} <button>like</button></p>
+          <p style={{margin: '0'}}>{blog.user.username}</p>
+        </div>
+      }
+    </div>
+  )
+}
+
+const BlogButton = ({ showDetails, toggle }) => (
+  <button onClick={toggle}>
+    { showDetails ? 'hide' : 'view'}
+  </button>
 )
 
 export default Blog
